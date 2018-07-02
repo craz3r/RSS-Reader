ebd63372905d537056adb8a4a9a42631ec1eac0f
“Sigov <nikit.sigov@gmail.com>
2018-07-02 16:24:39 +0300

    fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'index_bundle.js',
   },
   module: {
     rules: [
